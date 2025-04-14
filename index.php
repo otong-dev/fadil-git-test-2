a74334fd7d62f02fdb6dd81245a89cd6823a32dd
otong-dev <muhamadyuaiffadilah567@gmail.com>
2025-04-14 08:30:49 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello word</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, repellat provident. Consectetur aperiam ab dolorum. Vero, ea, consequatur quas aliquam, voluptate vitae laborum facere eaque accusamus tenetur inventore rem at?</p>
 </body>
 </html>
