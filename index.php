3feafcfdafa8658f0fb51e2e2b349e749666458c
otong-dev <muhamadyuaiffadilah567@gmail.com>
2025-04-14 08:40:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello word</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, repellat provident. Consectetur aperiam ab dolorum. Vero, ea, consequatur quas aliquam, voluptate vitae laborum facere eaque accusamus tenetur inventore rem at?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
